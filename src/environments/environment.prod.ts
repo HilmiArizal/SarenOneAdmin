ed3ee0129f69d31cfb04fe5acc85a5a352cb883b
Hilmi Arizal <hilmi.arizal36@gmail.com>
2021-08-31 14:25:56 +0700

    update-server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   API_URL:{
-    Local: 'https://sarenone-api.herokuapp.com/'
+    Local: 'http://localhost:8000/',
+    Server: 'https://sarenone-api.herokuapp.com/'
   }
 };
